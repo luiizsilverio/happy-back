a7ccf9ea9d2a8d16886272f88e9b643d81bcf6a1
luiizsilverio <luiiz.silverio@gmail.com>
2021-10-14 19:41:25 -0300

    Back-end do aplicativo Happy

---
+++ src/views/images_view.ts
@@ -0,0 +1,16 @@
+import Image from "../models/Image"
+
+export default {
+  render(image: Image) {
+    return {
+      id: image.id,
+      url: `http://localhost:3333/uploads/${image.path}`
+    }
+  },
+  
+  renderAll(images: Image[]) {
+    return images.map(image => this.render(image))
+  }
+}
+
+// url: `http://localhost:3333/uploads/${image.path}`
